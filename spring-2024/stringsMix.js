7a1b6bf0c3aa83286a572774c9fb07f1d48b856c
Benji Hix <benji.hix@outlook.com>
2024-03-23 15:18:51 -0700

    Perf | stringsMix
    First solution! 3 helper functions

--- spring-2024/stringsMix.js
+++ spring-2024/stringsMix.js
@@ -2,87 +2,80 @@
 //* or s2 appears as many times as its maximum if this maximum is strictly greater 
  //* than 1; these letters will be prefixed by the number of the string where they appear 
  //* with their maximum value and :. If the maximum is in s1 as well as in s2 the prefix is =:
-
-//~ pseudocode
-//` o) iterate through each string and record how often each lowercase appears
-//` o) sort each word's letter list based on frequency 
-//` 0) add all letter clusters to a string, taking into account which string
-//`     they come from
-
-// sort by .length 
-// use two queues simultaneously when adding to final string 
-
 function lowercaseClusters(str) {
-    const clusterObject = {};
-    const clusterArray = [];
+    const clusterObj = {};
+    const clusterArr = [];
     for (const char of str.replace(/[A-Z\s]/g, '')) {
-        char in clusterObject ? (clusterObject[char] += char) : (clusterObject[char] = char);
+        char in clusterObj ? (clusterObj[char] += char) : (clusterObj[char] = char);
     }
-
-    Object.keys(clusterObject).map((key) => {
-        if (clusterObject[key].length >= 2) {
-            clusterArray.push(clusterObject[key]);
+    // console.log(clusterObject);
+    Object.keys(clusterObj).map((key) => {
+        if (clusterObj[key].length >= 2) {
+            clusterArr.push(clusterObj[key]);
         }
     });
 
-    return clusterArray.sort((str1, str2) => {
-        if (str1.length === str2.length) {
-            return str2.localeCompare(str1);
-        } else {
-            return str1.length - str2.length;
+    return clusterArr;
+}
+
+function intersectClusterArrs(arr1, arr2) {
+    const combined = {}
+    for (const item of arr1) {
+        combined[item[0]] = ['1', item.length]
+    }
+    for (const item of arr2) {
+        if (item[0] in combined) {
+            if (item.length < combined[item[0]][1]) {
+                continue
+            }
+            else if (item.length > combined[item[0]][1]) {
+                combined[item[0]] = ['2', item.length]
+            }
+            else {
+                combined[item[0]] = ['=', item.length]
+            }
+        }
+        else {
+            combined[item[0]] = ['2', item.length]
+        }
+    }
+    const arr = [];
+    for (const [key, value] of Object.entries(combined)) {
+        arr.push([value[1], value[0], key])
+    }
+    return arr.sort( function (a, b) {
+        if ((a[0] === b[0]) && ((a[1] === '=' || b[1] === '=') && !(a[1] === '=' && b[1] === '='))) {
+            return b[1].localeCompare(a[1]);
+        }
+        else if (a[0] === b[0] && a[1] === b[1]) {
+            return a[2].localeCompare(b[2]);
+        }
+        else if (a[0] === b[0]) {
+            return (a[1].localeCompare(b[1]));
+        }
+        else {
+            return b[0] - a[0];
         }
     });
 }
 
+function clusterArrToString(arr) {
+    const stringsArr = arr.map( a => {
+        str = ''
+        str.concat(a[1]).concat(':').concat(a[2].repeat(a[0]));
+        return a[1].concat(':').concat(a[2].repeat(a[0]));
+    })
+    console.log(stringsArr);
+    return stringsArr.join('/')
+}
+
 function mix(s1, s2) {
-    let finalStrArr = []
-    const str1 = lowercaseClusters(s1);
-    const str2= lowercaseClusters(s2);
-    console.log(str1);
-    console.log(str2);
-    while (str1.length && str2.length) {
-        let Regex1 = new RegExp(`${str1[str1.length-1]}`, 'g')
-        let Regex2 = new RegExp(`${str2[str2.length-1]}`, 'g')
-        if (finalStrArr.join('').match(Regex1)) {
-            str1.pop();
-        }
-        else if (finalStrArr.join('').match(Regex2)) {
-            str2.pop();
-        }
-        else if (str1[str1.length-1].length > str2[str2.length-1].length) {
-            finalStrArr.push(`1:${str1.pop()}`);
-        }
-        else if (str1[str1.length-1].length < str2[str2.length-1].length) {
-            finalStrArr.push(`2:${str2.pop()}`);
-        }
-        else {
-            console.log(str1[str1.length-1]);
-            console.log(str2[str2.length-1]);
-            if (str1[str1.length-1] !== str2[str2.length-1]) {
-                if (str1[str1.length-1].localeCompare(str2[str2.length-1]) < 0) {
-                    finalStrArr.push(`1:${str1.pop()}`)
-                }
-                else {
-                    finalStrArr.push(`2:${str2.pop()}`)
-                }
-            }
-            else {
-                finalStrArr.push(`=:${str1.pop()}`);
-                str2.pop();
-            }
-        }
-    }
-    while (str1.length) {
-        finalStrArr.push(`1:${str1.pop()}`)
-    }
-    while (str2.length) {
-        finalStrArr.push(`2:${str2.pop()}`)
-    }
-    console.log(finalStrArr);
-    return finalStrArr.join('/')
+    const combinedClusters = intersectClusterArrs(lowercaseClusters(s1), lowercaseClusters(s2));
+    return clusterArrToString(combinedClusters);
  }
 
- console.log(mix("you are like a rat mm", "if my tweets seem untethered"));
+ console.log(mix("looping is fun but dangerous", "less dangerous than coding"));
+ console.log("1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg");
 
  //! Temporary pause; refactor necessary 
  //! At the moment, it's impossible for me to put =: cluster
